3bc20b33276fc9122f135dfdeacb37de015b8a8e
Phi Tran <phitranphitranphitran@gmail.com>
2016-05-12 14:16:08 -0400

    Disable UglifyJSPlugin warnings

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,6 @@ module.exports = {
         }),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
     ] : []
 };
