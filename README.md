86f1562011deaf5e5ca6e89a4e7a4ab78bdeb51c
Phi Tran <phitranphitranphitran@gmail.com>
2016-05-23 00:26:27 -0400

    Replace gulp-connect with simple express server, update readme

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Personal boilerplate for single page application web projects.
 - Basic folder structure for react-redux
 - webpack configured for babel ES6 transforming
 - webpack-dev-server with hot reloading
-- Gulp for compiling Sass, gulp-connect for heroku deployment
+- Gulp for compiling Sass, simple express server for deployment
 - redux store configured for chrome redux dev-tools extension
+- eslint, mocha, chai all ES6 ready
 
 Remember to npm install!
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
-var connect = require("gulp-connect");
 var production = process.env.NODE_ENV === "production";
 
 gulp.task("sass", function() {
@@ -14,15 +13,6 @@ gulp.task("sass", function() {
     // create sourcemap file
     .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest("./public/"));
-});
-
-// for deployment, Ex. heroku
-gulp.task("connect", function() {
-  connect.server({
-    root: "./public",
-    port: process.env.PORT || 3000,
-    livereload: false
-  });
 });
 
 // complete default task before watching
